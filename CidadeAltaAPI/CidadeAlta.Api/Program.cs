e6052e6af4f18f0841ec4b5abcadf95730f40b5b
Natan Fernandes <natanfernandes@protonmail.com>
2022-04-02 19:36:05 -0300

    add autenticação na ui do swagger

--- CidadeAltaAPI/CidadeAlta.Api/Program.cs
+++ CidadeAltaAPI/CidadeAlta.Api/Program.cs
@@ -12,6 +12,7 @@ using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,6 +28,33 @@ builder.Services.AddSwaggerGen(x =>
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     x.IncludeXmlComments(xmlPath);
+
+    x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme." +
+                      "\r\nEnter 'Bearer' [space] and then your token in the text input below." +
+                      "\r\nExample: 'Bearer 1234'"
+    });
+
+    x.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
 });
 
 builder.Services.AddDbContext<CidadeAltaContext>(options =>
@@ -88,7 +116,7 @@ app.MapControllers();
 app.Run();
 
 #region Injeção de dependências
-void InjetarDependencias(WebApplicationBuilder wab)
+static void InjetarDependencias(WebApplicationBuilder wab)
 {
     wab.Services.AddScoped<IAuthService, AuthService>();
     wab.Services.AddScoped<IAuthAppService, AuthAppService>();
